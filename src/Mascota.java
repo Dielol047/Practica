af508d8758c5f414f108f2d17f1783a0b1749f62
Dielol047 <diego.erazo.lascano@udla.edu.ec>
2026-04-21 17:14:41 -0500

    Avance 1

---
+++ src/Mascota.java
@@ -0,0 +1,12 @@
+public class Mascota {
+    private String nombre ;
+    private String especie ;
+    private int edad;
+
+    public Mascota (String nombre, String especie,int edad){
+        this.nombre= nombre;
+        this.especie=especie;
+        this.edad=edad;
+    }
+
+}
