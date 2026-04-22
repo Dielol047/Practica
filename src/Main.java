6e8397a3b8ecb8eb477a72ca70d65ac2195e4f6f
Dielol047 <diego.erazo.lascano@udla.edu.ec>
2026-04-21 21:50:23 -0500

    Avance 2

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,41 @@
 
     public class Main {
         public static void main(String[] args) {
-
-
+            
              Mascota mascota1 = new Mascota("Carlos", "Perro", 3);
             //solo pon los atributos y ya se autogenera
             //los String con comillas
 
-             Mascota mascota2 = new Mascota("Michi","Gato",2);
+             Mascota mascota2 = new Mascota("Cloe","Gato",2);
              //cualquier valor que no sea string va sin comillas
 
-            // 3. Imprime los datos
-            System.out.println("Mi mascotas es " +mascota1.obtenerResumen());
-            System.out.println("Mi mascotas es " +mascota1.obtenerResumen());
-            //Con obtener Resumen conseguimos los valores en privado
+            // 2. USO DE GETTERS (Para leer los datos)
+            System.out.println("--- INFORMACIÓN INICIAL ---");
+            System.out.println("La mascota 1 se llama: " + mascota1.getNombre());
+            System.out.println("La edad de la mascota 1 es: " + mascota1.getEdad());
+            // Supongamos que Carlos cumplió años y le cambiamos el nombre
+            mascota1.setEdad(4);
+            mascota1.setNombre("Carlos Junior");
+
+            System.out.println("¡Actualización realizada!");
+
+            // 4. VERIFICAMOS EL CAMBIO leyendo de nuevo con los Getters
+            System.out.println("\n--- INFORMACIÓN FINAL ---");
+            System.out.println("Nombre actualizado: " + mascota1.getNombre());
+            System.out.println("Edad actualizada: " + mascota1.getEdad());
+
+            mascota1.Animalmenor();
+            mascota2.Animalmenor();
+            //Como es void no retorna nada es más recomendable para la prueba
+            //solo llamas al metodo
 
 
-            
+
+
+
+
+
+
         }
     }
 
--- src/Mascota.java
+++ src/Mascota.java
@@ -2,18 +2,50 @@ public class Mascota {
     private String nombre ;
     private String especie ;
     private int edad;
-    //private opcional en la prueba
+    //private opcional en la prueba mejor public
 
     public Mascota (String nombre, String especie,int edad){
         this.nombre= nombre;
         this.especie=especie;
         this.edad=edad;
+        //todo contructor o metodo llama a la variable con this.variable
     }
-    // Metodo para utilizar los atributos en private
-    public String obtenerResumen() {
-        return "Nombre: " + this.nombre +
-                ", Especie: " + this.especie +
-                ", Edad: " + this.edad + " años.";
+    public void Animalmenor(){
+        if (this.edad<1){
+            System.out.println("La mascota de nombre"+this.nombre+"Esta en desarrollo priorizar nutricion");
+        }
+        else if (this.edad>1){
+            System.out.println("La mascota ya es adulta");
+        }
+
+        //todo contructor o metodo llama a la variable con this.variable
+
     }
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+
 
 }
